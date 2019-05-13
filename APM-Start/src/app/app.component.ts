4080a647cfca1c545966a1f83a1ef79069884c5e
Niteesh <niteesh.iit@gmail.com>
2019-05-13 16:54:54 -0400

    Data Binding, code enhancements 05/13/2019

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'pm-root',
+templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+
+  pageTitle: string = "Product Management";
+
 }
